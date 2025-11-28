b58b265ad499374de7b1ec18b2dd4cab732b1451
miladzarein <134214582+miladzarein@users.noreply.github.com>
2025-11-28 15:47:15 +0330

    def clean email
    make sure that email is unique to create account

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,11 +3,22 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
+
+
+
+
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, label="ایمیل")
     first_name = forms.CharField(max_length=30, required=True, label="نام")
     last_name = forms.CharField(max_length=30, required=True, label="نام خانوادگی")
 
+
     class Meta:
         model = User
         fields = ("username", "email", "first_name", "last_name", "password1", "password2")
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("این ایمیل قبلاً استفاده شده است.")
+        return email
