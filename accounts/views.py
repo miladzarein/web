ec9dcf1f279e5a8273f06363bed050e6bd6c331b
miladzarein <134214582+miladzarein@users.noreply.github.com>
2025-11-28 13:20:57 +0330

    created massage for check login
    created html for check if user is login

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 def login_view(request):
-    return render(request, 'accounts/login.html')
+    return render(request, 'accounts/login.html',)
 
 #def logout_view(request):
     #return render(request, 'accounts/logout.html')
